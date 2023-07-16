77de4bf5571a5667f63b4e86f621de3becfd6744
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-07-16 20:17:18 +0300

    photo

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -1,6 +1,6 @@
 import { Formik } from "formik";
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -21,11 +21,34 @@ import {
 } from "@expo-google-fonts/roboto";
 import { FontAwesome } from "@expo/vector-icons";
 import { EvilIcons } from "@expo/vector-icons"; 
+import { AntDesign } from "@expo/vector-icons"; 
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import { Image } from "react-native";
 
 export const CreatePostsScreen = () => {
   const navigation = useNavigation();
   const [nameFocus, setNameFocus] = useState(false);
   const [regionFocus, setRegionFocus] = useState(false);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState('');
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   
   let [fontsLoaded] = useFonts({
     Roboto_500Medium,
@@ -42,13 +65,29 @@ export const CreatePostsScreen = () => {
     region: "",
   };
 
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+    console.log("camera -->", photo.uri);
+    setPhoto(photo.uri);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.containerPhoto}>
         <View style={styles.photo}>
-          <View style={styles.containerIcon}>
-            <FontAwesome name="camera" size={24} color="#BDBDBD" />
-          </View>
+          <Camera style={styles.camera} ref={setCamera}>
+            {photo && (
+              <View style={styles.takePhotoContainer}>
+                <Image
+                  source={{ uri: photo }}
+                  style={{ height: 100, width: 100 }}
+                />
+              </View>
+            )}
+            <TouchableOpacity style={styles.containerIcon} onPress={takePhoto}>
+              <FontAwesome name="camera" size={24} color="#BDBDBD" />
+            </TouchableOpacity>
+          </Camera>
         </View>
         <Text style={styles.textUnderPhoto}>Завантажте фото</Text>
       </View>
@@ -76,7 +115,10 @@ export const CreatePostsScreen = () => {
                   />
                   <View style={styles.containerInput}>
                     <TextInput
-                      style={[styles.input, regionFocus && styles.inputFocused]}
+                      style={[
+                        styles.inputRegion,
+                        regionFocus && styles.inputFocused,
+                      ]}
                       value={values.region}
                       placeholder="Місцевість..."
                       onChangeText={handleChange("region")}
@@ -84,7 +126,7 @@ export const CreatePostsScreen = () => {
                       onBlur={() => setRegionFocus(false)}
                     />
                     <TouchableOpacity
-                      style={styles.textInInput}
+                      style={styles.iconLocation}
                       // onPress={handleVisiblePassword}
                     >
                       <EvilIcons name="location" size={24} color="#BDBDBD" />
@@ -102,6 +144,9 @@ export const CreatePostsScreen = () => {
           </KeyboardAvoidingView>
         </View>
       </TouchableWithoutFeedback>
+      <TouchableOpacity style={styles.iconDelete}>
+        <AntDesign name="delete" size={24} color="#BDBDBD" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -124,6 +169,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#F6F6F6",
     marginBottom: 8,
   },
+  camera: {
+    flex: 1,
+  },
   containerIcon: {
     flex: 1,
     justifyContent: "center",
@@ -136,6 +184,13 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 50,
     backgroundColor: "#FFFFFF",
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    borderColor: "#ffffff",
+    borderWidth: 1,
   },
   textUnderPhoto: {
     fontFamily: "Roboto_400Regular",
@@ -151,12 +206,26 @@ const styles = StyleSheet.create({
   input: {
     paddingTop: 16,
     paddingBottom: 16,
+    paddingLeft: 8,
     marginBottom: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#E8E8E8",
     fontSize: 16,
     fontFamily: "Roboto_400Regular",
     placeholderTextColor: "#BDBDBD",
+    borderRadius: 8,
+  },
+  inputRegion: {
+    paddingTop: 16,
+    paddingLeft: 28,
+    paddingBottom: 16,
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E8E8E8",
+    fontSize: 16,
+    fontFamily: "Roboto_400Regular",
+    placeholderTextColor: "#BDBDBD",
+    borderRadius: 8,
   },
   inputFocused: {
     borderWidth: 1,
@@ -166,17 +235,33 @@ const styles = StyleSheet.create({
   containerInput: {
     position: "relative",
   },
+  iconLocation: {
+    position: "absolute",
+    top: 22,
+  },
   button: {
-    marginTop: 43,
+    marginTop: 16,
     borderRadius: 100,
     paddingTop: 16,
     paddingBottom: 16,
-    backgroundColor: "#FF6C00",
+    backgroundColor: "#F6F6F6",
   },
   buttonText: {
     fontFamily: "Roboto_400Regular",
-    color: "#FFFFFF",
+    color: "#BDBDBD",
     fontSize: 16,
     textAlign: "center",
+  },
+  iconDelete: {
+    width: 70,
+    height: 40,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 23,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 20,
   },
 });
