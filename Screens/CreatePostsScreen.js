537359a9008e88a9507a5ca9742a665ba51bbc4c
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-07-16 10:33:34 +0300

    +

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -1,4 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Formik } from "formik";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import {
   useFonts,
   Roboto_500Medium,
@@ -6,8 +20,13 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { FontAwesome } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons"; 
 
 export const CreatePostsScreen = () => {
+  const navigation = useNavigation();
+  const [nameFocus, setNameFocus] = useState(false);
+  const [regionFocus, setRegionFocus] = useState(false);
+  
   let [fontsLoaded] = useFonts({
     Roboto_500Medium,
     Roboto_400Regular,
@@ -17,35 +36,93 @@ export const CreatePostsScreen = () => {
   if (!fontsLoaded) {
     return null;
   }
-  
+
+  const initialValue = {
+    name: "",
+    region: "",
+  };
+
   return (
-    <View style={[styles.containerPhoto, { backgroundColor: "#F6F6F6" }]}>
-      <View style={styles.photo}>
-        <View style={styles.containerIcon}>
-          <FontAwesome name="camera" size={24} color="#BDBDBD" />
+    <View style={styles.container}>
+      <View style={styles.containerPhoto}>
+        <View style={styles.photo}>
+          <View style={styles.containerIcon}>
+            <FontAwesome name="camera" size={24} color="#BDBDBD" />
+          </View>
         </View>
+        <Text style={styles.textUnderPhoto}>Завантажте фото</Text>
       </View>
-      <Text>Завантажте фото</Text>
+
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.rectangle}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <Formik
+              initialValues={initialValue}
+              onSubmit={(values) => {
+                navigation.navigate("Home", { values });
+              }}
+            >
+              {({ handleChange, handleSubmit, values }) => (
+                <View>
+                  <TextInput
+                    style={[styles.input, nameFocus && styles.inputFocused]}
+                    value={values.name}
+                    placeholder="Назва..."
+                    onChangeText={handleChange("name")}
+                    onFocus={() => setNameFocus(true)}
+                    onBlur={() => setNameFocus(false)}
+                  />
+                  <View style={styles.containerInput}>
+                    <TextInput
+                      style={[styles.input, regionFocus && styles.inputFocused]}
+                      value={values.region}
+                      placeholder="Місцевість..."
+                      onChangeText={handleChange("region")}
+                      onFocus={() => setRegionFocus(true)}
+                      onBlur={() => setRegionFocus(false)}
+                    />
+                    <TouchableOpacity
+                      style={styles.textInInput}
+                      // onPress={handleVisiblePassword}
+                    >
+                      <EvilIcons name="location" size={24} color="#BDBDBD" />
+                    </TouchableOpacity>
+                  </View>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleSubmit}
+                  >
+                    <Text style={styles.buttonText}>Опублікувати</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            </Formik>
+          </KeyboardAvoidingView>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containerPhoto: {
-    marginTop: 32,
-    marginBottom: 32,
-    marginLeft: 16,
-    marginRight: 16,
-    borderTopRightRadius: 8,
-    borderTopLeftRadius: 8,
+  container: {
+    width: "100%",
+    height: "100%",
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 32,
     backgroundColor: "#FFFFFF",
+  },
+  containerPhoto: {
+    marginBottom: 32,
   },
   photo: {
     height: 240,
     borderRadius: 8,
     backgroundColor: "#F6F6F6",
-    borderColor: "red",
-    borderBottomWidth: 1,
+    marginBottom: 8,
   },
   containerIcon: {
     flex: 1,
@@ -59,5 +136,47 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 50,
     backgroundColor: "#FFFFFF",
+  },
+  textUnderPhoto: {
+    fontFamily: "Roboto_400Regular",
+    fontSize: 16,
+    color: "#BDBDBD",
+  },
+  rectanglePhoto: {
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+    backgroundColor: "#F6F6F6",
+  },
+  input: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E8E8E8",
+    fontSize: 16,
+    fontFamily: "Roboto_400Regular",
+    placeholderTextColor: "#BDBDBD",
+  },
+  inputFocused: {
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    backgroundColor: "FFFFFF",
+  },
+  containerInput: {
+    position: "relative",
+  },
+  button: {
+    marginTop: 43,
+    borderRadius: 100,
+    paddingTop: 16,
+    paddingBottom: 16,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    fontFamily: "Roboto_400Regular",
+    color: "#FFFFFF",
+    fontSize: 16,
+    textAlign: "center",
   },
 });
